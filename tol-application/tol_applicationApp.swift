5a06c2f6da7b32eafc92b278b7fa667663c567a5
jacksonwiese <jacksonwiese1@gmail.com>
2023-04-13 08:57:27 -0500

    Initial Commit

---
+++ tol-application/tol_applicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  tol_applicationApp.swift
+//  tol-application
+//
+//  Created by Jackson Wiese on 4/13/23.
+//
+
+import SwiftUI
+
+@main
+struct tol_applicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
