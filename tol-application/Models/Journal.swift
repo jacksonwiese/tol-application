4c8f332d98f6ebb3105b531baca082dbc0ce3b66
jacksonwiese <jacksonwiese1@gmail.com>
2023-04-13 16:23:12 -0500

    updates

--- tol-application/Models/Journal.swift
+++ tol-application/Models/Journal.swift
@@ -19,7 +19,10 @@ struct TolThought: Identifiable, Codable {
 extension TolThought {
     static let sampleData: [TolThought] =
     [
-        TolThought(title: "I went to Purdue today", date: DateFormatter.date(from: "2023-03-19")!, lengthSeconds: 159, lengthWords: 391, transcript: "Today, I flew to Purdue to visit my best friend Jordan...")
+        TolThought(id: UUID(), title: "I went to Purdue today", date: DateFormatter().date(from: "2023-03-19 10:12:00")!, lengthSeconds: 159, lengthWords: 391, transcript: "Today, I flew to Purdue to visit my best friend Jordan..."),
+        TolThought(id: UUID(), title: "Thoughts on Purdue's campus", date: DateFormatter().date(from: "2023-03-19 12:56:00")!, lengthSeconds: 159, lengthWords: 391, transcript: "Today, I flew to Purdue to visit my best friend Jordan..."),
+        TolThought(id: UUID(), title: "I went to Purdue today", date: DateFormatter().date(from: "2023-03-19 17:37:00")!, lengthSeconds: 159, lengthWords: 391, transcript: "Today, I flew to Purdue to visit my best friend Jordan...")
+        
 //        DailyScrum(title: "Design",
 //                   attendees: ["Cathy", "Daisy", "Simon", "Jonathan"],
 //                   lengthInMinutes: 10,
